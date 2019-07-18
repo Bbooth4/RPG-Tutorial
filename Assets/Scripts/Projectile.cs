3d7eb22f1fc53d94bb781566620acccdf5ae67ab
brandon <booth.brandon4@gmail.com>
2019-07-17 20:18:34 -0400

    added a projectile

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile: MonoBehaviour {
+  Rigidbody2D rigidbody2D;
+
+  void Start() {
+    rigidbody2D = GetComponent<Rigidbody2D>();
+  }
+
+  void Update() {
+    
+  }
+
+  public void Launch(Vector2 direction, float force) {
+    rigidbody2d.AddForce(direction * force);
+  }
+
+  void OnCollisionEnter2D(Collision2D other) {
+    // we also add a debug log to know what the projectile touch
+    Debug.Log("Projectile Collision with " + other.gameObject);
+    Destroy(gameObject);
+  }
+}
